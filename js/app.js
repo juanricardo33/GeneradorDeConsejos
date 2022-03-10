3b2b122e597ccbe04930ca8e89992c1a265c0d49
juanricardo33 <42702796+juanricardo33@users.noreply.github.com>
2022-03-09 23:35:05 -0500

    DEPLOY

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// Variables y selectores
+const btnGenerarAdvice = document.querySelector(".btn")
+const textAdvice = document.querySelector("#textAdvice");
+const numberAdvice = document.querySelector("#numberAdvice")
+
+
+//Eventos
+eventListeners();
+function eventListeners(){
+    btnGenerarAdvice.addEventListener("click", obtenerDatos);
+}
+
+function obtenerDatos(){
+    //location.reload()
+    const url = "https://api.adviceslip.com/advice"
+    fetch(url)
+        .then(respuesta =>{
+            console.log(respuesta);
+            return respuesta.json()
+        })
+        .then( datos => {
+            mostrarHTML(datos)
+        })
+        .catch(error =>{
+            alert("Intentelo más tarde")
+        })
+}
+
+function mostrarHTML(datos){
+    const {id, advice} = datos.slip;
+    console.log(id);
+    console.log(advice);
+    numberAdvice.textContent = "ADVICE #" + id;
+    textAdvice.textContent = advice;
+}
